e3cafd81a4d06fd5d8a3d01fe4405e483c3a8366
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-09-19 21:03:20 +0530

    Introduction
    Hello World in C++

---
+++ C++Programming/Introduction/HelloWorld.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Rookr on 19/09/25.
+//
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World\n";
+    cout<<"Welcome to the world of C++";
+
+    return 0;
+}
