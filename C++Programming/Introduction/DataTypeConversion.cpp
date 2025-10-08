ad953876788bc359f46c89ed414cadd0493c2635
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-10-08 11:58:58 +0530

    Data Types in C++

---
+++ C++Programming/Introduction/DataTypeConversion.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Rookr on 08/10/25.
+//
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 3;
+    char c ='C';
+
+    // Convert char data type into integer
+    cout << (int)c << endl;
+    int sum = n + c;
+    cout << sum;
+    return 0;
+}
---
+++ C++Programming/Introduction/DataTypesinC++.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Rookr on 08/10/25.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Integers in C++
+    int var = 10;
+    cout << var;
+    // Character variable
+    char c = 'A';
+    cout << c;
+    // Creating an integer variable
+    int x = 25;
+    cout << x << endl;
+    // Using hexadecimal base value
+    x = 0x15;
+    cout << x;
+    // Creating a boolean variable
+    bool isTrue = true;
+    cout << isTrue;
+    // Floating point variable with a decimal value
+    float f = 36.5;
+    cout << f;
+    // double precision floating point variable
+    double pi = 3.1415926535;
+    cout << pi;
+
+
+    return 0;
+}
