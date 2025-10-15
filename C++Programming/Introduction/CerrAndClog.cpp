0b35a9742c0037dfe34cac18787c02dbd3cb6c16
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2025-10-15 17:03:00 +0530

    Input and Output

---
+++ C++Programming/Introduction/CerrAndClog.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Rookr on 15/10/25.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cerr << "An error occurred";
+    clog << "An error occurred";
+    return 0;
+}
